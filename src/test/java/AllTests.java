dba5b4d7439c7bea1797ce846127354a26c6b608
Yuri <91310724+istrebitelantox@users.noreply.github.com>
2022-12-16 13:58:18 +0300

    Commits added

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -21,45 +21,74 @@ public class AllTests {
     @Test()
     public void WebTableTest()
     {
-        webTables.openWindowAndForms();
+        //Открывается окно, а затем форма для заполнения
+        webTables.openWindow();
+        //Заполняется имя и фамилия работника
         webTables.nameInformation("Yuriy","Gruzdev");
+        //Заполняется e-mail работника
         webTables.emailInformation("dareon@gmail.com");
+        //Заполняется возраст работника
         webTables.ageInformation("19");
+        //Заполняется зарплата работника
         webTables.salaryInformation("10000");
+        //Заполняется подразделение работника
         webTables.departmentInformation("Test");
+        //Принимается информация о работнике и добавляется в таблицу
         webTables.inputSubmit();
     }
     @Test
     public void PracticeFormTest()
     {
-        practiceFormTest.openWindowAndForms();
+        //Открывается окно, а затем форма для заполнения
+        practiceFormTest.openWindow();
+        //Заполняется имя и фамилия студента
         practiceFormTest.nameInfo("Yuriy","Gruzdev");
+        //Заполняется e-mail студента
         practiceFormTest.emailInfo("dareon@gmail.com");
+        //Заполняется пол студента
         practiceFormTest.genderInfo();
+        //Заполняется мобильный телефон студента
         practiceFormTest.mobileInfo("89502458776");
+        //Заполняется дата рождения студента
         practiceFormTest.dayOfBirthInfo();
+        //Вставляется фотография студента
         practiceFormTest.inputPicture("C:\\Users\\инет\\IdeaProjects\\TestForMMTR\\2.jpg");
+        //Заполняется адресс студента
         practiceFormTest.currentAddressInfo("ул. Советска, 19");
+        //Принимается информация о студенте
         practiceFormTest.inputSubmit();
+        //Закрывается форма
+        practiceFormTest.inputClose();
     }
     @Test
     @Flaky
     public void downlaodAndUploadTest()
     {
+        //Открывается окно, а затем форма для заполнения
         downloadAndUpload.openWindow();
+        //Скачивается файл с сайта
         downloadAndUpload.downloadFile();
+        //Загружается своя фотография
         downloadAndUpload.uploadFile();
     }
     @Test
     public void linksTest()
     {
+        //Открывается окно, а затем форма
         links.openWindow();
+        //Открывается ссылка и проверятеся ответ
         links.createdCheck();
+        //Открывается ссылка и проверятеся ответ
         links.noContentCheck();
+        //Открывается ссылка и проверятеся ответ
         links.movedCheck();
+        //Открывается ссылка и проверятеся ответ
         links.badRequestCheck();
+        //Открывается ссылка и проверятеся ответ
         links.unauthorizedCheck();
+        //Открывается ссылка и проверятеся ответ
         links.forbiddenCheck();
+        //Открывается ссылка и проверятеся ответ
         links.notFoundCheck();
     }
 }
