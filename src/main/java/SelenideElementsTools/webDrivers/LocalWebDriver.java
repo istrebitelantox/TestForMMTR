4b2eb4275ce5204a0b7abf5da60e427a5e5f5a11
istrebitelantox <geekpook@gmail.com>
2023-02-14 17:40:58 +0300

    Update Jenkinsfile

--- src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
+++ src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
@@ -20,12 +20,33 @@ public class LocalWebDriver implements WebDriverProvider {
     @Override
     public org.openqa.selenium.WebDriver createDriver(@Nonnull Capabilities capabilities) {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options=new ChromeOptions()
-            .addArguments("--no-sandbox")
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--disable-logging");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-translate");
+        options.addArguments("--disable-plugins");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--enable-automation");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--no-first-run");
+        options.addArguments("--silent");
+        options.addArguments("--start-maximized");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--headless");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-setuid-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--proxy-server='direct://'");
+        options.addArguments("--proxy-bypass-list=*");
+/*            .addArguments("--no-sandbox")
             .addArguments("--disable-gpu")
             .addArguments("--window-size=1920,1080")
             .addArguments("--headless")
-            .addArguments("--disable-dev-shm-usage");
+            .addArguments("--disable-dev-shm-usage")
+            .addArguments("--ignore-certificate-errors")*/
         options.setCapability(PAGE_LOAD_STRATEGY, "eager");
 
         Map<String, Object> prefs = new HashMap<>();
