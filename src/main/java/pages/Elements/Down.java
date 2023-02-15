466e6065ad60e6da5ee413d84f4eedd467c69405
istrebitelantox <geekpook@gmail.com>
2023-02-15 09:09:38 +0300

    Update Jenkinsfile

--- src/main/java/pages/Elements/Down.java
+++ src/main/java/pages/Elements/Down.java
@@ -10,7 +10,7 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 
 public class Down {
-    public File file=new File("data/sampleFile.jpeg");
+    public File file=new File("src/main/resources/avatar.jpg");
     public BasePage window = new BasePage("Elements");
     public Forms downloadanduploadform = new Forms("Upload and Download");
     public A downlaodButton = new A("downloadButton");
