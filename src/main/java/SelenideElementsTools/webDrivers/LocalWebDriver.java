b2d1de51a722b8243990ee59acbbdbb1dcdcfdfc
istrebitelantox <geekpook@gmail.com>
2023-02-16 09:33:35 +0300

    Update jenkins adn docker files

--- src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
+++ src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
@@ -20,7 +20,6 @@ public class LocalWebDriver implements WebDriverProvider {
     @Override
     public org.openqa.selenium.WebDriver createDriver(@Nonnull Capabilities capabilities) {
         WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
         ChromeOptions options=new ChromeOptions();
         options.addArguments("--disable-logging");
         options.addArguments("--disable-infobars");
