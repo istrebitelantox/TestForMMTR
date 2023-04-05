e6e97dd88ecdd2bfec909a0953b5b571fd6f7b84
istrebitelantox <geekpook@gmail.com>
2023-04-05 15:46:20 +0300

    Update LocalWebDriver

--- src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
+++ src/main/java/SelenideElementsTools/webDrivers/LocalWebDriver.java
@@ -16,45 +16,23 @@ import java.util.Map;
 import static org.openqa.selenium.remote.CapabilityType.PAGE_LOAD_STRATEGY;
 
 public class LocalWebDriver implements WebDriverProvider {
+    public WebDriver driver;
     @Nonnull
     @Override
-    public org.openqa.selenium.WebDriver createDriver(@Nonnull Capabilities capabilities) {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options=new ChromeOptions();
-        options.addArguments("--disable-logging");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-translate");
-        options.addArguments("--disable-plugins");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-web-security");
-        options.addArguments("--enable-automation");
-        options.addArguments("--no-default-browser-check");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--no-first-run");
-        options.addArguments("--silent");
-        options.addArguments("--start-maximized");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--disable-gpu");
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-setuid-sandbox");
+        options.addArguments("--lang=ru");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--proxy-server='direct://'");
-        options.addArguments("--proxy-bypass-list=*");
-/*            .addArguments("--no-sandbox")
-            .addArguments("--disable-gpu")
-            .addArguments("--window-size=1920,1080")
-            .addArguments("--headless")
-            .addArguments("--disable-dev-shm-usage")
-            .addArguments("--ignore-certificate-errors")*/
-        options.setCapability(PAGE_LOAD_STRATEGY, "eager");
+
+        options.setCapability(PAGE_LOAD_STRATEGY, "normal");
 
         Map<String, Object> prefs = new HashMap<>();
-        prefs.put("download.default_directory", new File(".").getAbsolutePath() + "/data");
+        prefs.put("download.default_directory", new File(".").getAbsolutePath() + "data");
         options.setExperimentalOption("prefs", prefs);
 
-        WebDriver driver=new ChromeDriver(options);
-        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(420));
+        WebDriverManager.chromedriver().driverVersion("110.0.5481.77").setup();
+        driver = new ChromeDriver(options);
         return driver;
     }
 }
